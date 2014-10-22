dbc63978168f3c9133441348bc2fde1364f651d2
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-22 09:27:40 -0400

    Setting up a Dig button to do something when clicked

--- app/src/main/java/com/detroitlabs/icandigit/fragments/MapFragment.java
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/MapFragment.java
@@ -7,15 +7,26 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.detroitlabs.icandigit.R;
 
 public class MapFragment extends Fragment {
 
+    private Button mDigButton;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_map, null);
+
+        mDigButton = (Button) rootView.findViewById(R.id.button_digit);
+        mDigButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
         return rootView;
     }
 }
